1b26a8b8ca9683c1f6241e580fa95028c1b3eb3b
Grbisba <polickarpow.dany@yandex.ru>
2025-04-15 22:45:00 +0300

    add routers

--- src/routers/v1/__init__.py
+++ src/routers/v1/__init__.py
@@ -1,6 +1,18 @@
 from fastapi import APIRouter
 
 from .ml.ml import router as ml
+from .ml.generate_ai_test import router as generate_ai_test
+from .ml.scientific_test import router as scientific_test
+from .ml.personality_test import router as personality_test
+from .ml.summarize import router as summarize
 
 router = APIRouter(prefix="/v1")
+
 router.include_router(ml, prefix="/ml", tags=["ml"])
+
+router.include_router(generate_ai_test, prefix="/ai_test", tags=["AI Test"])
+
+router.include_router(scientific_test, prefix="/test", tags=["Test"])
+router.include_router(personality_test, prefix="/test", tags=["Test"])
+
+router.include_router(summarize, prefix="/summary", tags=["Summary"])
