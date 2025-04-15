c0aaa3228b066d9f7193eed1cd55907533d78497
Grbisba <polickarpow.dany@yandex.ru>
2025-04-15 14:54:55 +0300

    change bullshit

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@ import logging
 
 import uvicorn
 from fastapi import FastAPI
+from uvicorn import config
 
 from routers.v1.ml.scientific_test import router as scientific_test_router
 from routers.v1.ml.personality_test import router as personality_test_router
 from routers.v1.ml.generate_ai_test import router as ai_test_router
 from routers.v1.ml.summarize import router as summarize_router
 from settings import settings
-
 
 app = FastAPI()
 
@@ -22,12 +22,17 @@ app.include_router(summarize_router, prefix="/summarize", tags=["Summarize"])
 
 
 async def main():
-    print(settings.ADDR, settings.PORT)
-    uvicorn.run("main:app",
-                host=settings.ADDR,
-                port=settings.PORT,
-                reload=settings.TEST,
-                log_level=settings.LOG_LEVEL)
+    cfg = config.Config(
+        "main:app",
+        host=settings.ADDR,
+        port=settings.PORT,
+        reload=settings.TEST,
+        log_level=settings.LOG_LEVEL
+    )
+
+    server = uvicorn.Server(cfg)
+
+    await server.serve()
 
 
 if __name__ == "__main__":
--- src/settings.py
+++ src/settings.py
@@ -1,24 +1,30 @@
 import os
 
-from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=os.getenv("CONFIG_PATH"),
+        validate_default=True
+    )
+
     ADDR: str = "127.0.0.1"
-    PORT: int = 8080
+    PORT: int = 8083
     TEST: bool = True
 
     LOG_LEVEL: str = "info"
-    
+
     MODEL_TYPE: str = 'pro'
     LLM_TEMPERATYRE: float = 0.8
     LLM_MAX_TOKENS: int = 1000
 
     FOLDER_ID: str = ""
     IAM_TOKEN: str = ""
-
-    class Config:
-        env_file = os.getenv("CONFIG_PATH")
 
 
 settings = Settings()
