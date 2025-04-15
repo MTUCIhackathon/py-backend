07264a8289178466b76927a2b6233ee5ac77be91
Grbisba <polickarpow.dany@yandex.ru>
2025-04-15 22:39:54 +0300

    fix imports in src/routers/v1

--- src/routers/v1/__init__.py
+++ src/routers/v1/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from .wml.ml import router as ml
+from .ml.ml import router as ml
 
 router = APIRouter(prefix="/v1")
 router.include_router(ml, prefix="/ml", tags=["ml"])
